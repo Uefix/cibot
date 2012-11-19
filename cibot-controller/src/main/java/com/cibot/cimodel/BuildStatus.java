06bb3d3495676122b948ebdd7f886be4e18a2abd
Thomas Gatzka <thomas.gatzka@tga-it.com>
2012-11-19 23:28:12 +0100

    BuildStatus + Logo Jenkins

--- cibot-controller/src/main/java/com/cibot/cimodel/BuildStatus.java
+++ cibot-controller/src/main/java/com/cibot/cimodel/BuildStatus.java
@@ -2,10 +2,14 @@ package com.cibot.cimodel;
 
 /**
  * Defines the possible states a single CI build can adopt.
- * 
+ *
+ * @author Uefix
  * @author j-n00b
  */
 public enum BuildStatus {
+
+    /** Indicates that the status is unknown and can't be determined. */
+    UNKNOWN,
 
     /** Indicates that the build is okay and stable */
     BUILD_OK,
@@ -15,4 +19,6 @@ public enum BuildStatus {
 
     /** Indicates that the build is broken because the code does not compile */
     BUILD_FAILED;
+
+
 }
